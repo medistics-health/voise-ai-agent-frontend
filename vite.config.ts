5cc8dc8c0e6573659a7549e93994decf32488c6b
pakabari84 <pakabari@medisticshealth.com>
2026-03-17 19:29:43 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: [
+      '46cc-103-240-204-165.ngrok-free.app'
+    ],
     port: 5173,
     proxy: {
       '/api': {
